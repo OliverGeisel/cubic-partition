4e08137d169d986f998442cff9ad29be08963424
OliverGeisel <geisel.oliver@googlemail.com>
2021-02-16 16:03:24 +0100

    Add new notebook Partition.ipynb and add first testfiles

---
+++ test/src/core/test_Solution.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestPartition(TestCase):
+    def test_to_bi_point_list(self):
+        self.fail()
---
+++ test/src/core/test_solver.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class Test_solver(TestCase):
+    def test_move_x_percent(self):
+        self.fail()
